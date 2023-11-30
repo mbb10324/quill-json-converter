d81f65ce9fdf7e38eb9dccf0fba7996816e1c182
Miles <breman12@yahoo.com>
2023-11-30 16:16:37 -0500

    added qol buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "react-quill/dist/quill.snow.css";
 import ReactQuill from "react-quill";
-import "./App.css";
 import { useState } from "react";
+import "./App.css";
 
 export default function App() {
 	const [delta, setDelta] = useState();
+	const [copied, setCopied] = useState(false);
 
 	function handleQullJSON(content, delta, source, editor) {
 		const theDelta = editor.getContents();
@@ -14,15 +15,27 @@ export default function App() {
 		}
 	}
 
+	function copyJsonToClipboard() {
+		const deltaJson = JSON.stringify(delta, null, 2);
+		navigator.clipboard.writeText(deltaJson);
+		setCopied(true);
+		setTimeout(() => {
+			setCopied(false);
+		}, 3000);
+	}
+
 	return (
 		<div className="app">
 			<h1>Quill JSON Converter</h1>
 			<div className="quill-wrapper">
-				<ReactQuill theme="snow" onChange={handleQullJSON} placeholder="Start Typing..." />
+				<ReactQuill theme="snow" onChange={handleQullJSON} placeholder="Start Typing..." value={delta} />
+				<button onClick={() => setDelta()}>Reset Editor</button>
 				<div className="quill-json">
 					<h2>Output:</h2>
 					<pre>{JSON.stringify(delta, null, 2)}</pre>
 				</div>
+				<button onClick={copyJsonToClipboard}>Copy Json To Clipboard</button>
+				{copied && <span>Copied To Clipboard!</span>}
 			</div>
 		</div>
 	);
